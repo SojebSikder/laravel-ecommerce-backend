1384ed4b254e7ef82a7e674c61ac3d7bd5460fb3
sojebsikder <sojebsikder@gmail.com>
2023-01-11 19:50:38 +0600

    added custom attribute to product model

--- app/Models/Product/Product.php
+++ app/Models/Product/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Product;
 
+use App\Helper\SettingHelper;
 use App\Models\Category\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,6 +10,38 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $appends = ['currency_sign', 'currency_code', 'availability'];
+
+    // custom currency attribute
+    public function getCurrencySignAttribute()
+    {
+        return SettingHelper::currency_sign();
+    }
+
+    // custom currency code attribute
+    public function getCurrencyCodeAttribute()
+    {
+        return SettingHelper::currency_code();
+    }
+
+    // custom availability attribute
+    public function getAvailabilityAttribute()
+    {
+        $outOfStock = 'out of stock';
+        $inStock = 'in stock';
+        $totalQuantity = 0;
+
+        if ($this->quantity > 0) {
+            $totalQuantity +=  $this->quantity;
+        }
+
+        if ($totalQuantity > 0) {
+            return $inStock;
+        } else {
+            return $outOfStock;
+        }
+    }
 
     public function images()
     {
