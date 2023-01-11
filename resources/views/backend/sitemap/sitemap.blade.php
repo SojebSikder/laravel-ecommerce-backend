b0a0ba7da6655f9ed6a54214d94029c4440abc92
sojebsikder <sojebsikder@gmail.com>
2023-01-11 22:18:34 +0600

    main

--- resources/views/backend/sitemap/sitemap.blade.php
+++ resources/views/backend/sitemap/sitemap.blade.php
@@ -13,12 +13,6 @@
         <priority>0.8</priority>
     </url>
     @endforeach
-    @foreach ($products as $product)
-    <url>
-        <loc>{{ env('CLIENT_APP_URL') }}/products?c={{ $product->category->id }}@if(isset($product->category->sublink))&amp;menu={{ $product->category->sublink->id }}@endif</loc>
-        <priority>0.8</priority>
-    </url>
-    @endforeach
     {{-- pages --}}
     @foreach ($pages as $page)
     <url>
