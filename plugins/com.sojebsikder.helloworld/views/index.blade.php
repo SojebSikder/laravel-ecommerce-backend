9a000202e678c37f5092f1d13b49d860a7fd97ad
sojebsikder <sojebsikder@gmail.com>
2023-07-31 22:39:24 +0600

    updated plugin

--- plugins/com.sojebsikder.helloworld/views/index.blade.php
+++ plugins/com.sojebsikder.helloworld/views/index.blade.php
@@ -1,28 +1,94 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('backend.master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@section('title')
+    Demo
+@endsection
 
-<body>
-    <h1>Hello world</h1>
-    {{-- simple calculator --}}
-    <form action="{{ route('hello') }}" method="post">
-        @csrf
-        <input type="text" name="num1" placeholder="Enter first number">
-        <input type="text" name="num2" placeholder="Enter second number">
-        <button type="submit">Calculate</button>
-    </form>
+@section('style')
+@endsection
 
-    {{-- show result --}}
-    @if (Session::has('result'))
-        <strong>{{ Session::get('result') }}</strong>.
-    @endif
+@section('content')
+    <!-- Main section -->
+    <main class="mt-5 pt-3">
+        {{-- display error message --}}
+        @if (Session::has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert"
+                style="
+           margin: 10px 5px 10px 5px;">
+                <strong>{{ Session::get('success') }}</strong>.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @elseif (Session::has('warning'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert"
+                style="
+           margin: 10px 5px 10px 5px;">
+                <strong>{{ Session::get('warning') }}</strong>.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        @endif
+        {{-- //display error message --}}
 
-</body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 fw-bold fs-3">
+                    Demo
+                </div>
+            </div>
+            <div class="row">
+                <div class="row">
+                    <div class="col-md-12 mt-3">
+                        <div class="card">
+                            <div class="card-header"></div>
+                            <div class="card-body">
+                                <div>
+                                    {{-- simple calculator --}}
+                                    <form action="{{ route('calculate') }}" method="post">
+                                        @csrf
+                                        
+                                        <div class="col">
+                                            <div class="form-group mb-3">
+                                                <label for="num1">Enter first number</label>
+                                                <input type="text" id="num1"
+                                                    class="form-control @error('num1') is-invalid @enderror"
+                                                    value="{{ old('num1') }}" name="num1">
+                                            </div> 
+                                        </div>
 
-</html>
+                                        <div class="col">
+                                            <div class="form-group mb-3">
+                                                <label for="num2">Enter second number</label>
+                                                <input type="text" id="num2"
+                                                    class="form-control @error('num2') is-invalid @enderror"
+                                                    value="{{ old('num2') }}" name="num2">
+                                            </div> 
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col">
+                                                <div class="form-group mb-3">
+                                                    <button id="submit" type="submit"
+                                                        class="btn btn-primary mt-3">Calculate</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                    {{-- show result --}}
+                                    @if (Session::has('result'))
+                                        <strong>{{ Session::get('result') }}</strong>.
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </main>
+    <!-- End Main section -->
+@endsection
+
+@section('script')
+@endsection
