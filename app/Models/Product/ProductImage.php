dbfff11413acabc3b3ac63c04aad1ba692a391d5
sojebsikder <sojebsikder@gmail.com>
2023-12-11 15:32:13 +0600

    hidden fields

--- app/Models/Product/ProductImage.php
+++ app/Models/Product/ProductImage.php
@@ -10,6 +10,18 @@ class ProductImage extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'status',
+    ];
+
+
     protected $appends = ['image_url'];
 
     public function getImageUrlAttribute()
